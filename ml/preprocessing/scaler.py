9d1fa04fb7f249fecc647481de625ef108d7fbc0
Graia Ramin <142338131+Ramine92@users.noreply.github.com>
2026-03-19 01:16:02 +0100

    added a scaler class that scale numerical_columns using StandardScaler or MinMaxScaler

---
+++ ml/preprocessing/scaler.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+class Scaler:
+    def __init__(self,num_cols,method="standard"):
+        self.num_cols = num_cols
+        #used later in MinMaxScaler
+        self.min_ = {}
+        self.max_ = {} 
+        #used later in StandardScaler
+        self.mean_ = {}
+        self.std_ = {}
+        #strategy to use 
+        self.method = method
+    
+    def fit(self,X):
+        X = X.copy()
+        self.m = len(X)
+        for col in self.num_cols:
+            if self.method == "minmax":
+                self.min_[col] = X[col].min()
+                self.max_[col] = X[col].max()
+            else:
+                self.mean_[col] = X[col].mean()
+                self.std_[col] = np.sqrt((1/self.m)*np.sum((X[col]-self.mean_[col])**2))
+        return self
+    
+    def transform(self,X):
+        X = X.copy()
+        if self.method == "minmax":
+            for col in self.num_cols:
+                X[col] = (X[col]-self.min_[col])/(self.max_[col]-self.min_[col])
+        else:
+            for col in self.num_cols:
+                X[col] = (X[col]-self.mean_[col])/self.std_[col]
+        return X
