d2907a25523f9d1b0b6be6b15b470b85012168ee
Graia Ramin <142338131+Ramine92@users.noreply.github.com>
2026-03-24 17:42:46 +0100

    feat:added the automlpipeline which train evaluate and store the best model / registry.py stores all the models name whether its classification model or regression model

---
+++ ml/models/registry.py
@@ -0,0 +1,5 @@
+from ml.models.Regression.LinearRegression import LinearRegression
+from ml.models.classification.LogisticRegression import LogisticRegression
+
+CLASSIFICATION_MODELS = [logisticRegression]
+REGRESSION_MODELS = [LinearRegression]
---
+++ ml/pipelines/auto_ml_pipeline.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from ml.data.data_manager import DataManager
+from ml.preprocessing import Preprocessor 
+from ml.models.registry import REGRESSION_MODELS,CLASSIFICATION_MODELS
+class AutoMLPipeline:
+    
+    def __init__(self,target_column):
+        self.target_column = target_column
+        self.best_model_ = None # stores the best model 
+    
+    def run(self,file_path):
+        #loading and spliting data
+        data_manager = DataManager(self.target_column)
+        df,num_cols,cat_cols,isclassificaion = data_manager.load_and_profile(file_path=file_path)
+        X_train,X_test,y_train,y_test = data_manager.get_split()
+        #preprocessing data
+        preprocessor = Preprocessor(cat_cols,num_cols)
+        X_train_clean = preprocessor.fit_transform(X_train)
+        X_test_clean = preprocessor.transform(X_test)
+        #training model and evaluating best model
+        if isclassificaion == False:
+            score = -1
+            for CLASSMODELS in REGRESSION_MODELS:
+                model = CLASSMODELS()
+                model.fit(X_train_clean,y_train)
+                tmpscore = model.score(X_test_clean,y_test)
+                if tmpscore > score :
+                    self.best_model_ = model
+                    score = tmpscore
+
+        else:
+            score = -1
+            for CLASSMODELS in CLASSIFICATION_MODELS:
+                model = CLASSMODELS()
+                model.fit(X_train,y_train)
+                tmpscore = model.score(X_test_clean,y_test)
+                if tmpscore > score:
+                    self.best_model_ = model
+                    score = tmpscore
+        
+
+    def predict(self,X_new):
+        return self.best_model_.predict(X_new)
+
+
+
+
